8b4500845ef51d17ea82ae8bf86a541f9825f6a5
Abhinav Rastogi <rastogi.abhinav8@gmail.com>
2021-11-28 18:34:09 +0530

    Frontend Added

--- src/App.js
+++ src/App.js
@@ -86,29 +86,42 @@ class App extends Component {
       }
       this.simpleStorageInstance.set(result[0].hash, { from: this.state.account }).then((r) => {
         return this.setState({ ipfsHash: result[0].hash })
-        console.log('ifpsHash', this.state.ipfsHash)
+        // console.log('ifpsHash', this.state.ipfsHash)
       })
     })
   }
 
   render() {
     return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-          <a href="#" className="pure-menu-heading pure-menu-link">IPFS File Upload DApp</a>
+      <div className="App felx-col space-y-4">
+        <nav className="w-full h-max bg-white shadow-md p-2 flex items-center space-x-8">
+          <div href="#" className="flex-col text-lg text-black w-20 h-20 p-2 border-border border-2 rounded flex justify-center items-start ml-4 text-left">
+            <div className="brand1 font-bold">BLOCK</div>
+            <div className="brand1 font-medium">DOC</div>
+            </div>
+          <div className="nav-txt text-black text-2xl font-medium"><b>BlockChain </b>Storage for your secured <b> documents</b></div>
         </nav>
 
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Your Image</h1>
-              <p>This image is stored on IPFS & The Ethereum Blockchain!</p>
-              <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
-              <h2>Upload Image</h2>
-              <form onSubmit={this.onSubmit} >
-                <input type='file' onChange={this.captureFile} />
-                <input type='submit' />
+        <main className="container w-full h-max mb-12 ">
+          <div className=" flex w-full justify-center items-center h-screen">
+            <div className="sub-cont flex w-3/4 h-screen justify-center items-center p-2">
+              
+              <div className="left-div w-1/2">
+             
+              <form onSubmit={this.onSubmit} className="w-96 h-96 rounded shadow-lg border-t-4 border-black p-4 space-y-12 flex-col justify-center">
+                <p className="form-txt text-lg font-medium">Upload your document (.jpg,.png)</p>
+                <input className="w-full h-8 rounded-md border-black border-2"type='file' onChange={this.captureFile} />
+                <input type='submit' className="w-28 h-10 bg-black text-white rounded-md p-2 hover:bg-gray-800" />
               </form>
+              </div>
+              <div className="right-div w-96 h-96 flex-col text-black shadow-lg border-t-4 border-black  ">
+                <div className="img-sec rounded-lg  p-4 flex-col justify-center ">
+                  <p className="text-md font-medium">This image is stored on <b>IPFS</b> & The <b>Ethereum Blockchain</b>!</p>
+                  <div className="img-wrap w-64 h-64 border-gray-600 border-2 rounded-lg ">
+                  <img src={ `https://ipfs.io/ipfs/${this.state.ipfsHash}` } alt="img" width="70%" h="auto" />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </main>
